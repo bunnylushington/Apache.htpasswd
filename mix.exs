9b30d10cca0ed12fd61718219ccd4797a65fba70
Kevin Montuori <montuori@gmail.com>
2014-09-29 06:39:22 -0500

    added hex info

--- mix.exs
+++ mix.exs
@@ -5,10 +5,30 @@ defmodule Htpasswd.Mixfile do
     [app: :htpasswd,
      version: "0.0.1",
      elixir: "~> 1.0.0",
+     description: description,
+     package: package,
      deps: deps]
   end
+
   def application do
     [applications: [:logger]]
+  end
+  
+  def description do 
+    """ 
+    Provides basic htpasswd(1) functions as a library: encode and
+    check passwords in MD5, SHA, crypt, or plaintext format, add to
+    and delete from htaccess files.
+    """
+  end
+
+  def package do
+    [
+     files: ["lib", "mix.exs", "README*", "test"],
+     contributors: ["Kevin Montuori"],
+     licenses: ["MIT"],
+     links %{"GitHub" => "https://github.com/kevinmontuori/Apache.htpasswd"}
+    ]
   end
 
   defp deps do
